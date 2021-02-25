fdb18fd9a3c533fa7862eaf799484bf0ab9e6679
Scott Gall <scottarthurgall@gmail.com>
2021-02-25 16:58:44 -0600

    changed lower on input string

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-show = input("what show or movie are you looking for? --> ").lower()
+show = input("what show or movie are you looking for? --> ")
 
 csvpath = os.path.join("resources", "netflix-ratings.csv")
 
@@ -11,7 +11,7 @@ with open(csvpath, newline="") as csvfile:
   csvreader = csv.reader(csvfile, delimiter=",")
 
   for line in csvreader:
-    if line[0].lower() == show:
+    if line[0].lower() == show.lower():
       print("{} is rated {} with a rating of {}.".format(line[0], line[1], line[3]))
       found = True
       break
