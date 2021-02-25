ef8805a3c24ff38c5a959670e8791c94d27112e9
Scott Gall <scottarthurgall@gmail.com>
2021-02-25 16:02:43 -0600

    added code and gitignore

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import csv
+
+show = input("what show or movie are you looking for? --> ").lower()
+
+csvpath = os.path.join("resources", "netflix-ratings.csv")
+
+found = False
+
+with open(csvpath, newline="") as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=",")
+
+  for line in csvreader:
+    if line[0].lower() == show:
+      print("{} is rated {} with a rating of {}.".format(line[0], line[1], line[3]))
+      found = True
+      break
+
+  if found is False:
+    print("Sorry, we didn't find {}.".format(show))
